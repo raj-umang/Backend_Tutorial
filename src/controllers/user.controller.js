602b6b6060ea85712c4b8c273c883776695c1c99
Umang Raj <jackerumang@gmail.com>
2024-01-29 23:07:13 +0530

    Add getWatchHistory endpoints

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -441,12 +441,65 @@ const getUserChannelProfile = asyncHandler(async(req,res) => {
             "User Channel fetched successfully"
         )
     )
+})
 
+const getWatchHistory = asyncHandler(async(req, res) => {
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "user",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user[0].watchHistory,
+            "Watch history fetched successfully"
+        )
+    )
 })
 
 export {
     changeCurrentPassword,
-    getCurrentUser, getUserChannelProfile, loginUser,
+    getCurrentUser, getUserChannelProfile, getUserChannelProfile, getWatchHistory, getWatchHistory, loginUser,
     logoutUser,
     refreshAccessToken,
     registerUser,
